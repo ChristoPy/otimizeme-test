b33c3e7d8668a4a5a19da4a4200a956d91c2c6fd
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-09-19 07:26:30 -0300

    Clean up files

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppLogo from '@/components/AppLogo.vue'
+import ShoppingCartButton from '@/components/ShoppingCartButton.vue'
+</script>
+
+<template>
+    <header class="fixed w-full z-30 top-0 py-1 bg-white">
+        <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
+            <AppLogo />
+            <ShoppingCartButton />
+        </div>
+    </header>
+</template>
