cf2d20f94770ab8bd4dbea0383f41bdc280a1b07
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-09-19 17:58:26 -0300

    Add payment form

---
+++ src/components/AddressCardForm.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Card from '@/components/Card.vue';
+import BaseInput from '@/components/BaseInput.vue';
+</script>
+
+<template>
+  <Card class="mb-8">
+    <BaseInput id="name" label="Nome completo" placeholder="Digite seu nome completo" />
+    <BaseInput id="email" label="E-mail" placeholder="Digite seu e-mail" type="email" />
+    <div class="flex gap-4">
+      <BaseInput id="phone" label="Telefone" placeholder="(00) x0000-0000" type="tel" />
+      <BaseInput id="zipCode" label="CEP" placeholder="Digite seu CEP" type="tel" />
+    </div>
+    <BaseInput id="address" label="Endereço" placeholder="Digite seu endereço completo" />
+    <div class="flex gap-4">
+      <BaseInput id="number" label="Número" placeholder="Número" type="tel" />
+      <BaseInput id="complement" label="Complemento" placeholder="Digite seu complemento" />
+    </div>
+    <BaseInput id="neighborhood" label="Bairro" placeholder="Digite seu bairro" />
+    <div class="flex gap-4">
+      <BaseInput id="city" label="Cidade" placeholder="Digite sua cidade" />
+      <BaseInput id="state" label="Estado" placeholder="Digite seu estado" />
+    </div>
+  </Card>
+</template>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import CartItem from '@/components/CartItem.vue';
 import Card from '@/components/Card.vue';
-import BaseInput from '@/components/BaseInput.vue';
+import AddressCardForm from '@/components/AddressCardForm.vue';
+import PaymentCardForm from '@/components/PaymentCardForm.vue';
 </script>
 
 <template>
@@ -9,28 +10,12 @@ import BaseInput from '@/components/BaseInput.vue';
     <div class="container mx-auto max-w-5xl">
       <h1 class="font-bold text-4xl my-4 py-8">Finalizar Compra</h1>
 
-      <Card class="mb-4">
+      <Card class="mb-8">
         <CartItem v-for="item of $store.state.cart.all" :key="item.product._id" :item="item" :allowChanges="false" />
       </Card>
 
-      <Card>
-        <BaseInput id="name" label="Nome completo" placeholder="Digite seu nome completo" />
-        <BaseInput id="email" label="E-mail" placeholder="Digite seu e-mail" type="email" />
-        <div class="flex gap-4">
-          <BaseInput id="phone" label="Telefone" placeholder="(00) x0000-0000" type="tel" />
-          <BaseInput id="zipCode" label="CEP" placeholder="Digite seu CEP" type="tel" />
-        </div>
-        <BaseInput id="address" label="Endereço" placeholder="Digite seu endereço completo" />
-        <div class="flex gap-4">
-          <BaseInput id="number" label="Número" placeholder="Número" type="tel" />
-          <BaseInput id="complement" label="Complemento" placeholder="Digite seu complemento" />
-        </div>
-        <BaseInput id="neighborhood" label="Bairro" placeholder="Digite seu bairro" />
-        <div class="flex gap-4">
-          <BaseInput id="city" label="Cidade" placeholder="Digite sua cidade" />
-          <BaseInput id="state" label="Estado" placeholder="Digite seu estado" />
-        </div>
-      </Card>
+      <AddressCardForm />
+      <PaymentCardForm />
     </div>
   </section>
 </template>
