1f1fb4ac0f333c2131e977dda7d21e3dd4dc7001
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-09-19 17:28:32 -0300

    Prepare checkout form

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const { onChange } = defineProps({
+  label: String,
+  type: String,
+  id: String,
+  onChange: Function,
+  placeholder: String,
+});
+
+const handleChange = (event: Event) => {
+  if (onChange) {
+    onChange((event.target as HTMLInputElement).value);
+  }
+};
+</script>
+
+<template>
+  <div class="w-full">
+    <label class="block font-medium mb-2 w-full" :for="id">{{ label }}</label>
+    <input required :type="type || 'text'" :id="id" :placeholder="placeholder"
+      class="border border-gray-300 hover:border-gray-500 rounded h-10 px-2 mb-4 w-full" @input="handleChange" />
+  </div>
+</template>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import CartTotal from '@/components/CartTotal.vue';
 import CartItem from '@/components/CartItem.vue';
 import Card from '@/components/Card.vue';
+import BaseInput from '@/components/BaseInput.vue';
 </script>
 
 <template>
@@ -9,9 +9,27 @@ import Card from '@/components/Card.vue';
     <div class="container mx-auto max-w-5xl">
       <h1 class="font-bold text-4xl my-4 py-8">Finalizar Compra</h1>
 
-      <Card>
+      <Card class="mb-4">
         <CartItem v-for="item of $store.state.cart.all" :key="item.product._id" :item="item" :allowChanges="false" />
-        <CartTotal />
+      </Card>
+
+      <Card>
+        <BaseInput id="name" label="Nome completo" placeholder="Digite seu nome completo" />
+        <BaseInput id="email" label="E-mail" placeholder="Digite seu e-mail" type="email" />
+        <div class="flex gap-4">
+          <BaseInput id="phone" label="Telefone" placeholder="(00) x0000-0000" type="tel" />
+          <BaseInput id="zipCode" label="CEP" placeholder="Digite seu CEP" type="tel" />
+        </div>
+        <BaseInput id="address" label="Endereço" placeholder="Digite seu endereço completo" />
+        <div class="flex gap-4">
+          <BaseInput id="number" label="Número" placeholder="Número" type="tel" />
+          <BaseInput id="complement" label="Complemento" placeholder="Digite seu complemento" />
+        </div>
+        <BaseInput id="neighborhood" label="Bairro" placeholder="Digite seu bairro" />
+        <div class="flex gap-4">
+          <BaseInput id="city" label="Cidade" placeholder="Digite sua cidade" />
+          <BaseInput id="state" label="Estado" placeholder="Digite seu estado" />
+        </div>
       </Card>
     </div>
   </section>
