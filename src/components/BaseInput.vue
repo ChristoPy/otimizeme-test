6a1d0cf5be5ad73b857bcc93da93ac15166b1249
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-09-20 08:23:55 -0300

    Fix inputs reactivity

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,23 +1,31 @@
 <script setup lang="ts">
-const { onChange } = defineProps({
+import { ref, watch } from 'vue';
+
+const { modelValue } = defineProps({
   label: String,
   type: String,
   id: String,
-  onChange: Function,
   placeholder: String,
+  modelValue: String,
 });
 
-const handleChange = (event: Event) => {
-  if (onChange) {
-    onChange((event.target as HTMLInputElement).value);
-  }
-};
+const internalValue = ref(modelValue);
+const emit = defineEmits(['input']);
+
+watch(internalValue, (newVal) => {
+  emit('input', newVal);
+});
+
+function emitInput() {
+  emit('input', internalValue.value);
+}
 </script>
 
 <template>
   <div class="w-full">
     <label class="block font-medium mb-2 w-full" :for="id">{{ label }}</label>
     <input required :type="type || 'text'" :id="id" :placeholder="placeholder"
-      class="border border-gray-300 hover:border-gray-500 rounded h-10 px-2 mb-4 w-full" @input="handleChange" />
+      class="border border-gray-300 hover:border-gray-500 rounded h-10 px-2 mb-4 w-full" v-model="internalValue"
+      @input="emitInput" />
   </div>
 </template>
--- src/components/MaskedInput.vue
+++ src/components/MaskedInput.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-import { ref, watch, defineEmits } from 'vue';
+import { ref, watch } from 'vue';
 
-const { value } = defineProps({
+const { modelValue, mask } = defineProps({
   label: String,
   type: String,
   id: String,
   placeholder: String,
-  mask: String,
-  value: String
+  mask: [String, Array],
+  modelValue: String,
+  maxLength: Number,
 });
 
-const internalValue = ref(value);
+const internalValue = ref(modelValue);
 const emit = defineEmits(['input']);
 
-watch(() => value, () => {
-  if (value !== internalValue.value) {
-    internalValue.value = value;
-  }
-  emit('input', internalValue.value);
+watch(internalValue, (newVal) => {
+  emit('input', newVal);
 });
+
+function emitInput() {
+  emit('input', internalValue.value);
+}
 </script>
 
 <template>
   <div class="w-full">
     <label class="block font-medium mb-2 w-full" :for="id">{{ label }}</label>
     <input required :type="type || 'text'" :id="id" :placeholder="placeholder" v-mask="mask"
-      class="border border-gray-300 hover:border-gray-500 rounded h-10 px-2 mb-4 w-full" v-model="internalValue" />
+      class="border border-gray-300 hover:border-gray-500 rounded h-10 px-2 mb-4 w-full" v-model="internalValue"
+      @input="emitInput" />
   </div>
 </template>
