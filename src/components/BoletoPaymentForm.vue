fb8a4811f04f311dff9be1b9c40a2f1adeffc4bc
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-09-21 10:05:34 -0300

    Update payment methods infos

---
+++ src/components/BoletoPaymentForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="mx-auto text-gray-500 mb-8">Atenção:</div>
+
+    <div class="flex gap-4">
+      <div class="p-4 bg-white rounded-xl flex flex-col justify-center items-center gap-2 border border-gray-300 w-1/2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="33" viewBox="0 0 32 33" fill="none">
+          <path
+            d="M16 8.83321V16.8332L21.3333 19.4999M29.3333 16.8332C29.3333 24.197 23.3638 30.1665 16 30.1665C8.63616 30.1665 2.66663 24.197 2.66663 16.8332C2.66663 9.46941 8.63616 3.49988 16 3.49988C23.3638 3.49988 29.3333 9.46941 29.3333 16.8332Z"
+            stroke="#939598" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <p class="text-gray-500 text-center">Boletos levam até 3 dias úteis para serem compensados e então terem os
+          produtos liberados.</p>
+      </div>
+
+      <div class="p-4 bg-white rounded-xl flex flex-col justify-center items-center gap-2 border border-gray-300 w-1/2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="33" viewBox="0 0 32 33" fill="none">
+          <path
+            d="M18.3252 3.84465L28.3626 10.3689C28.7173 10.5995 28.8946 10.7147 29.0231 10.8685C29.1368 11.0046 29.2223 11.162 29.2744 11.3316C29.3333 11.5231 29.3333 11.7346 29.3333 12.1576V21.9332C29.3333 24.1734 29.3333 25.2935 28.8973 26.1492C28.5138 26.9018 27.9019 27.5137 27.1493 27.8972C26.2936 28.3332 25.1735 28.3332 22.9333 28.3332H9.06663C6.82642 28.3332 5.70631 28.3332 4.85066 27.8972C4.09802 27.5137 3.48609 26.9018 3.1026 26.1492C2.66663 25.2935 2.66663 24.1734 2.66663 21.9332V12.1576C2.66663 11.7346 2.66663 11.5231 2.72552 11.3316C2.77765 11.162 2.86309 11.0046 2.97682 10.8685C3.10531 10.7147 3.28264 10.5995 3.63732 10.3689L13.6747 3.84465M18.3252 3.84465C17.4836 3.29757 17.0628 3.02403 16.6093 2.9176C16.2085 2.82353 15.7914 2.82353 15.3906 2.9176C14.9372 3.02403 14.5163 3.29757 13.6747 3.84465M18.3252 3.84465L26.5815 9.2112C27.4986 9.80734 27.9572 10.1054 28.116 10.4834C28.2548 10.8137 28.2548 11.186 28.116 11.5164C27.9572 11.8944 27.4986 12.1924 26.5815 12.7886L18.3252 18.1551C17.4836 18.7022 17.0628 18.9757 16.6093 19.0822C16.2085 19.1762 15.7914 19.1762 15.3906 19.0822C14.9372 18.9757 14.5163 18.7022 13.6747 18.1551L5.41844 12.7886C4.50131 12.1924 4.04274 11.8944 3.88392 11.5164C3.74513 11.186 3.74513 10.8137 3.88392 10.4834C4.04274 10.1054 4.50131 9.80734 5.41844 9.2112L13.6747 3.84465M28.6666 25.6665L19.8095 17.6665M12.1904 17.6665L3.33329 25.6665"
+            stroke="#939598" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <p class="text-gray-500 text-center">Depois do pagamento, fique atento ao seu e-mail para receber os dados de
+          acesso ao produto (verifique também a caixa de SPAM)</p>
+      </div>
+
+      <div class="p-4 bg-white rounded-xl flex flex-col justify-center items-center gap-2 border border-gray-300 w-1/2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="33" viewBox="0 0 32 33" fill="none">
+          <path
+            d="M28 13.6665H4M21.3333 2.99988V8.33321M10.6667 2.99988V8.33321M10.4 29.6665H21.6C23.8402 29.6665 24.9603 29.6665 25.816 29.2306C26.5686 28.8471 27.1805 28.2352 27.564 27.4825C28 26.6269 28 25.5068 28 23.2665V12.0665C28 9.82633 28 8.70623 27.564 7.85058C27.1805 7.09793 26.5686 6.48601 25.816 6.10252C24.9603 5.66654 23.8402 5.66654 21.6 5.66654H10.4C8.15979 5.66654 7.03969 5.66654 6.18404 6.10252C5.43139 6.48601 4.81947 7.09793 4.43597 7.85058C4 8.70623 4 9.82633 4 12.0665V23.2665C4 25.5068 4 26.6269 4.43597 27.4825C4.81947 28.2352 5.43139 28.8471 6.18404 29.2306C7.03969 29.6665 8.15979 29.6665 10.4 29.6665Z"
+            stroke="#939598" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <p class="text-gray-500 text-center">A cada renovação da assinatura enviaremos um novo boleto para que você possa
+          pagar. Assim, você fica sempre em dia sem se preocupar.</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/PaymentCardForm.vue
+++ src/components/PaymentCardForm.vue
@@ -3,12 +3,16 @@ import Card from '@/components/Card.vue'
 import CreditCardPaymentForm from './CreditCardPaymentForm.vue';
 import PaymentMethodOptions from './PaymentMethodOptions.vue';
 import SecurityNotice from './SecurityNotice.vue';
+import PixPaymentForm from './PixPaymentForm.vue';
+import BoletoPaymentForm from './BoletoPaymentForm.vue';
 </script>
 
 <template>
   <Card>
     <PaymentMethodOptions />
     <CreditCardPaymentForm v-if="$store.getters['checkout/paymentMethod'] === 'credit-card'" />
+    <PixPaymentForm v-if="$store.getters['checkout/paymentMethod'] === 'br-pix'" />
+    <BoletoPaymentForm v-if="$store.getters['checkout/paymentMethod'] === 'br-boleto'" />
     <SecurityNotice />
   </Card>
 </template>
