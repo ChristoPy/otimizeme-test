147cf4ae87f4d66373810af63b44c122972daafb
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-09-21 12:17:40 -0300

    Update installments dropdown

--- src/components/InstallmentsDropdown.vue
+++ src/components/InstallmentsDropdown.vue
@@ -5,22 +5,28 @@ import { formatMoney } from '@/utils/money'
 import { ref } from 'vue';
 import store from '@/store'
 
-import Dropdown from './Dropdown.vue';
-
 const numberOfInstallments = 10;
 const totalPrice = store.getters['checkout/totalPrice'];
 
-const dropdownOptions = ref<DropdownOption[]>(
-  Array.from({ length: numberOfInstallments }, (_, i) => ({
-    id: `${i + 1}`,
-    name: `${i + 1}x de ${formatMoney(totalPrice / (i + 1))}`
-  }))
-);
+const dropdownOptions: DropdownOption[] = Array.from({ length: numberOfInstallments }, (_, i) => ({
+  id: `${i + 1}`,
+  name: `${i + 1}x de ${formatMoney(totalPrice / (i + 1))}`
+}));
 
-let internalValue = ref<DropdownOption>(dropdownOptions.value[0]);
+let internalValue = ref<DropdownOption>(dropdownOptions[0]);
+
+function emitInput() {
+  store.dispatch('checkout/setPaymentDetails', { installments: internalValue.value })
+}
 </script>
 
 <template>
-  <Dropdown id="installments" label="Parcelas" :placeholder="internalValue.name" :options="dropdownOptions"
-    v-model="internalValue" />
+  <div class="w-full">
+    <label class="block font-medium mb-2 w-full" for="installments">Parcelas</label>
+    <select id="installments" class="border border-gray-300 hover:border-gray-500 rounded h-10 px-2 mb-4 w-full"
+      @change="emitInput" v-model="//@ts-ignore
+        internalValue.id">
+      <option v-for="(option, index) in dropdownOptions" :key="option.id" :value="option.id">{{ option.name }}</option>
+    </select>
+  </div>
 </template>
